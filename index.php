af1914a97ca944548136bbc81dea582ee340150f
Mark Buckle <markbuckle92@gmail.com>
2024-09-16 15:27:53 -0230

    index update

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 include("database.php");
 
+$username = "Patrick";
+$password = "rock3";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
 // write a query within a variable. Users is the database. User and password are columns.
 $sql = "INSERT INTO users (user, password)
-            VALUES ('Spongebob', 'pineapple1')";
+        VALUES ('$username', '$hash')";
 
-// submit the query. Pass in the connection and the query.
-mysqli_query($conn, $sql);
-
+// try blocks allow you to give back different messages if the code breaks / fails
+try {
+    // submit the query. Pass in the connection and the query.
+    mysqli_query($conn, $sql);
+    echo "User is now registered";
+}
+// if we encounter a problem, let's catch it
+catch (mysqli_sql_exception) {
+    echo "Could not register user";
+}
 // close the connection
 mysqli_close($conn);
