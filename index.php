9d6339957486209e28729d91e330080a09c08bff
Mark Buckle <markbuckle92@gmail.com>
2024-09-16 14:51:25 -0230

    index update

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
-
 <?php
-    include("database.php");
+include("database.php");
 
-    // write a query within a variable. Users is the database. User and password are columns.
-    $sql = "INSERT INTO users (user, password)
+// write a query within a variable. Users is the database. User and password are columns.
+$sql = "INSERT INTO users (user, password)
             VALUES ('Spongebob', 'pineapple1')";
 
-    // submit the query. Pass in the connection and the query.
-    mysqli_query($conn, $sql);
+// submit the query. Pass in the connection and the query.
+mysqli_query($conn, $sql);
 
-    // close the connection
-    mysqli_close($conn);
-?>
+// close the connection
+mysqli_close($conn);
